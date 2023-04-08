75d37826e9d9885995980cb135246dceea895e2a
Jorge Acosta <lycantroponatural@gmail.com>
2023-04-08 02:41:42 -0500

    primer commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    // const [count, setCount] = useState(0);
+
+    return (
+        <div className="App">
+            <h1>LLEGE2</h1>
+        </div>
+    );
+}
+
+export default App;
